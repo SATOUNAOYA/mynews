430beee51dd0386b7112f4d087ae7d4d51f5c1f8
”sato” <naoya120328@icloud.com>
2020-01-07 05:27:35 +0000

    laravel

--- web.php
+++ web.php
@@ -1,7 +1,7 @@
-
+<?php
 Route::group(['prefix' => 'admin'], function() {
-Route::get('news/admin',Admin\ProfileController@edit');
-Route::get('news/profile',Admin/ProfileController@edit');
-Route::get('news/edit',Admin/ProfileController@edit');
+Route::get('news/admin','Admin\ProfileController@edit');
+Route::get('news/profile','Admin\ProfileController@edit');
+Route::get('news/edit','Admin\ProfileController@edit');
 });
 
